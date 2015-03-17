29dc8160e6e546afba9f02cc6f0741a3b8afe74c
Akbar Hidayat <keripix@gmail.com>
2015-03-17 17:35:34 +0700

    bismillah. utak atik

---
+++ app.js
@@ -0,0 +1,44 @@
+(function(global, $) {
+
+	// Model
+	var AppStateModel = (function() {
+		var appState = {};
+
+		return {
+			setState: function(key, value) {
+				appState[key] = value;
+
+				$(document).trigger('appstate:changed', [key, value]);
+			},
+			constructState: function(stateObj) {
+				appState = JSON.parse(stateObj);
+
+				$(document).trigger('appstate:constructed', [appState]);
+			},
+			getStateString: function() {
+				return JSON.stringify(appState);
+			}
+		};
+	})();
+
+	// Presenter-like functionalities
+	$(document).on('keyup', '[data-hook="app-state"]', function(e) {
+		var $target = $(e.target);
+		AppStateModel.setState($target.attr('name'), $target.val());
+	});
+
+	$(document).on('appstate:constructed', function(e, stateObj) {
+		$.each(stateObj, function(name, value) {
+			$('input[name="' + name + '"]').val(value);
+		});
+	});
+
+	$('#appstringsubmit').click(function() {
+		AppStateModel.constructState($('#appstring').val());
+	});
+
+	$('#appstringget').click(function() {
+		$('#appstring').val(AppStateModel.getStateString());
+	});
+
+})(this, $);
